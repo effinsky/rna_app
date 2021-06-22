802a2088a8a0478c709d7555d97930a5b5942030
adi effinsky <adiforka@gmail.com>
2021-06-22 10:17:40 +0200

    modified Location component to use getCurrentPosition instead of watchPosition

--- components/Location.tsx
+++ components/Location.tsx
@@ -3,28 +3,24 @@ import { StyleSheet, Text, Platform, ScrollView as View } from "react-native"
 import { check, request, PERMISSIONS, RESULTS } from "react-native-permissions"
 import Geolocation from "react-native-geolocation-service"
 
-// types for GeolocationResponse
-export type LocationType = {
-  coords: CoordsType
+export interface LocationResponseType {
   mocked: boolean
   provider: string
   timestamp: number
-}
-
-type CoordsType = {
-  accuracy: number
-  altitude: number
-  altitudeAccuracy: number
-  heading: number
-  latitude: number
-  longitude: number
-  speed: number
+  coords: {
+    accuracy: number
+    altitude: number
+    altitudeAccuracy: number
+    heading: number
+    latitude: number
+    longitude: number
+    speed: number
+  }
 }
 
 const Location = () => {
-  // type incoming object?
   const [locationResponse, setLocationResponse] =
-    useState<null | LocationType>(null)
+    useState<null | LocationResponseType>(null)
 
   const permission =
     Platform.OS === "android"
@@ -32,44 +28,36 @@ const Location = () => {
       : PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
 
   useEffect(() => {
-    let watchID: number
-
     check(permission).then(result => {
       if (result === RESULTS.GRANTED) {
-        watchID = enableLocation()
+        enableLocation()
       } else if (result === RESULTS.DENIED) {
         request(permission).then(requestResult => {
           if (requestResult === RESULTS.GRANTED) {
-            watchID = enableLocation()
+            enableLocation()
           }
         })
       }
     })
 
     function enableLocation() {
-      const watchID = Geolocation.watchPosition(
+      Geolocation.getCurrentPosition(
         // making TS happy
         position => {
-          setLocationResponse(position as Required<LocationType>)
+          setLocationResponse(position as Required<LocationResponseType>)
         },
         err => console.error(err.code, err.message),
         {
           enableHighAccuracy: true,
           accuracy: { android: "high" },
           distanceFilter: 0,
-          interval: 1000,
-          fastestInterval: 500,
-          showsBackgroundLocationIndicator: true,
           showLocationDialog: true,
         },
       )
-      return watchID
     }
-
-    return () => {
-      Geolocation.clearWatch(watchID)
-    }
+    // no cleanup necessary withGetCurrentPosition
   }, [permission])
+  console.log(locationResponse)
 
   return (
     <View>
